fa4460717750868ca1b992c644287bdd2c9b059b
queen-leksa <74290635+queen-leksa@users.noreply.github.com>
2022-10-29 17:50:06 +0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "docs"),
 		filename: "main.js",
-		publicPath: "/"
+		publicPath: ""
 	},
 	devServer: {
 		static: path.resolve(__dirname, "./docs"),
